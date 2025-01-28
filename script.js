5704607bf062630b4b50a73319ec94f92e7ebc39
Mateo <hg097870pq@gmail.com>
2025-01-27 20:20:51 -0500

    script prueba

---
+++ script.js
@@ -0,0 +1,8 @@
+let buton=document.querySelector("button")
+
+buton.addEventListener("click",()=>{
+    let color1=Math.floor(Math.random(1)*255)
+    let color2=Math.floor(Math.random(1)*255)
+    let color3=Math.floor(Math.random(1)*255)
+    document.body.style.background=`rgba(${color1},${color2},${color3})`
+})
